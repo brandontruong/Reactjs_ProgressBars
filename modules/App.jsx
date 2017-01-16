703058cd0338b82df2c24c43b7517ae82dccbfb4
Brandon Truong <brandon-truong@hotmail.com>
2017-01-16 23:12:33 +1100

    implemented progress bar project

--- modules/App.jsx
+++ modules/App.jsx
@@ -1,76 +1,80 @@
 import React from 'react';
 
 class App extends React.Component {
-	constructor(){
-        super();
-        this.state = {
+	constructor() {
+		super();
+		this.state = {
 			bars: [],
 			buttons: [],
-            limit: 0,
+			limit: 0,
 			selectedBar: 0
-        };
+		};
 		this.handleChange = this.handleChange.bind(this);
 		this.handleBarChange = this.handleBarChange.bind(this);
-    };
-	
+	};
+
 	handleChange(event) {
 		var cBars = this.state.bars.slice();
 		var newValue = parseInt(cBars[this.state.selectedBar]) + parseInt(event.target.value);
-		if (newValue < 0) {newValue = 0;}
+		if (newValue < 0) {
+			newValue = 0;
+		}
 		cBars[this.state.selectedBar] = newValue;
 		this.setState({bars: cBars});
-	  }
-	  handleBarChange(event) {
+	}
+
+	handleBarChange(event) {
 		this.setState({selectedBar: event.target.value});
-	  }
-  
+	}
+
 	componentDidMount() {
-        fetch("http://frontend-exercise.apps.staging.digital.gov.au/bars")
-            .then( (response) => {
-                return response.json() })   
-                    .then( (json) => {
-						if (json !== undefined)
-						{
-							this.setState({bars: json.bars, buttons: json.buttons, limit: json.limit});
-						}
-                   });
-    };
-	
-   render() {
-		
-		var cBars = this.state.bars.map(function(item, index){
+		fetch("http://frontend-exercise.apps.staging.digital.gov.au/bars")
+			.then((response) => {
+				return response.json()
+			})
+			.then((json) => {
+				if (json !== undefined) {
+					this.setState({bars: json.bars, buttons: json.buttons, limit: json.limit});
+				}
+			});
+	};
+
+	render() {
+		var cBars = this.state.bars.map(function(item, index) {
 			const divStyle = {
-			  color: 'red',
-			  width: (item * 100)/this.state.limit + '%'
+				color: 'red',
+				width: (item * 100)/this.state.limit + '%'
 			};
-		
-		  return <div className={"progress-bar " + (this.state.limit < item ? 'overlimit' : '')} key={index} value={item}>
-					{item}
-					<div className="progressing" style={divStyle}></div>
+
+			return <div className={"progress-bar " + (this.state.limit < item ? 'overlimit' : '')} key={index} value={item}>
+				{item}
+				<div className="progressing" style={divStyle}>
 				</div>
+			</div>
 		}.bind(this));
-		var cOptions = this.state.bars.map(function(item, index){
-		  return <option key={index} value={index}>#progress{index+1}</option>
+
+		var cOptions = this.state.bars.map(function(item, index) {
+			return <option key={index} value={index}>#progress{index+1}</option>
 		});
 
 		var cButtons = this.state.buttons.map(function(item, index) {
 			return (
-					<button key={'button' + index.toString()} value={item} onClick={this.handleChange}>{item}</button>
-				);
-			}.bind(this));
-		
-      return (
-         <div>
-			<h1>Progress Bars Demo</h1>
-			<h1>Limit: {this.state.limit}</h1>
-			<div>{cBars}</div>
+				<button key={'button' + index.toString()} value={item} onClick={this.handleChange}>{item}</button>
+			);
+		}.bind(this));
+
+		return (
 			<div>
-				<select onChange={this.handleBarChange}>{cOptions}</select>
-				<div className="buttons">{cButtons}</div>
+				<h1>Progress Bars Demo</h1>
+				<h1>Limit: {this.state.limit}</h1>
+				<div>{cBars}</div>
+				<div>
+					<select onChange={this.handleBarChange}>{cOptions}</select>
+					<div className="buttons">{cButtons}</div>
+				</div>
 			</div>
-         </div>
-      );
-   }
-   
+		);
+	}
 }
+
 export default App;
