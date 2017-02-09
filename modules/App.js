f09cf92133c82e777aeef00d62b7e3cbda47b6a5
Brandon Truong <Brandon.Truong@lionco.com>
2017-02-09 17:16:18 +1100

    implemented sinon service call

--- modules/App.js
+++ modules/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
 import ProgressBarApplication from './ProgressBarApplication.js';
-import fetchData from './fetchData.js';
+//import fetchData from './fetchData.js';
 
+export const BASE_URL = 'http://frontend-exercise.apps.staging.digital.gov.au/bars';
+export const fetchData = () => {
+	return axios.get(`${BASE_URL}`);
+}
 class App extends React.Component {
+	
 	constructor() {
 		super();
 		this.state = {
+			loaded: false,
 			bars: [],
 			buttons: [],
 			limit: 0,
@@ -14,10 +21,14 @@ class App extends React.Component {
 	};
 
 	componentDidMount() {
-		 fetchData(json => {
+		this.props.fetchData()
+		.then(res => {
+			this.setState({loaded: true, bars: res.data.bars, buttons: res.data.buttons, limit: (res.data.limit === undefined)? 100: res.data.limit});
+		})
+		 /*fetchData(json => {
 	      if (json !== undefined) {
 	          this.setState({bars: json.bars, buttons: json.buttons, limit: json.limit});
-	    	}});
+	    	}});*/
 	};
 
 	render() {
@@ -30,5 +41,7 @@ class App extends React.Component {
 		);
 	}
 }
+
+App.defaultProps = { fetchData: fetchData };
 
 export default App;
