bb9f50fdc0830d799221550d5e325e5226ae0fa0
Brandon Truong <brandon-truong@hotmail.com>
2017-02-11 13:04:31 +1100

    added new test for ProgressBarApplication-test

--- __tests__/ProgressBarApplication-test.js
+++ __tests__/ProgressBarApplication-test.js
@@ -9,10 +9,13 @@ describe('ProgressBarApplication test', function() {
 	it('should mount in a full DOM', function() {
 		expect(mount(<ProgressBarApplication />).find('.progress-bar-application').length).to.equal(1);
 	});
-	it('componentWillReceiveProps called', async () => {
-		const data = {"buttons":[23,25,-8,-26],"bars":[81,82,35,46]};
-		const progressBarApplication = mount(<ProgressBarApplication data={data} />);
-		//expect(progressBarApplication.state('bars')).to.equal(data.bars);
-		//expect(progressBarApplication.state('buttons')).to.equal(data.buttons);
-	});
+  it('componentWillReceiveProps called', () => {
+    const data = {"buttons":[23,25,-8,-26],"bars":[81,82,35,46]};
+    const spy = sinon.spy(ProgressBarApplication.prototype, 'componentWillReceiveProps');
+    const progressBarApplication = mount(<ProgressBarApplication />);
+    expect(spy.calledOnce).to.equal(false);
+    progressBarApplication.setProps({ data: data });
+    expect(spy.calledOnce).to.equal(true);
+    expect(progressBarApplication.state('bars')).to.equal(data.bars);
+  });
 });
